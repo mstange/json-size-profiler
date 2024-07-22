1d2a498a2ddc68d561547e95083bad7acdf8fdae
Markus Stange <mstange.moz@gmail.com>
2024-07-22 18:55:37 -0400

    Clean up call tree.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use std::{
 };
 
 use fxprof_processed_profile::{
-    CategoryColor, CategoryHandle, CpuDelta, Frame, FrameFlags, FrameHandle, FrameInfo, Profile,
-    ReferenceTimestamp, SamplingInterval, StackHandle, ThreadHandle, Timestamp,
+    CategoryColor, CategoryHandle, CpuDelta, Frame, FrameFlags, FrameInfo, Profile,
+    ReferenceTimestamp, SamplingInterval, StackHandle, StringHandle, ThreadHandle, Timestamp,
 };
 use parser::{JsonPrimitiveValue, JsonSessionObserver};
 
@@ -73,17 +73,21 @@ struct State {
     profile: Profile,
     thread: ThreadHandle,
     stack: Vec<StackHandle>,
+    stack_labels: Vec<(StringHandle, CategoryHandle)>,
+    top_label: StringHandle,
+    top_category: CategoryHandle,
+    string_stack: Vec<String>,
     scope_stack: Vec<Scope>,
-    object_category: CategoryHandle,
-    f_obj: FrameHandle,
-    f_arr: FrameHandle,
-    f_null: FrameHandle,
-    f_bool: FrameHandle,
-    f_number: FrameHandle,
-    f_str: FrameHandle,
+    cat_obj: CategoryHandle,
+    cat_arr: CategoryHandle,
+    cat_null: CategoryHandle,
+    cat_bool: CategoryHandle,
+    cat_number: CategoryHandle,
+    cat_str: CategoryHandle,
     last_pos: u64,
     aggregate_depth: usize,
     aggregation_map: Option<(u64, HashMap<StackHandle, u64>)>,
+    array_depth: usize,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -101,65 +105,33 @@ impl State {
         );
         let process = profile.add_process("Bytes", 0, Timestamp::from_nanos_since_reference(0));
         let thread = profile.add_thread(process, 0, Timestamp::from_nanos_since_reference(0), true);
-        let object_category = profile.add_category("Object", CategoryColor::DarkGray);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<object>")),
-            category_pair: object_category.into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_obj = profile.intern_frame(thread, f);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<array>")),
-            category_pair: profile.add_category("Array", CategoryColor::Gray).into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_arr = profile.intern_frame(thread, f);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<null>")),
-            category_pair: profile.add_category("Null", CategoryColor::Yellow).into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_null = profile.intern_frame(thread, f);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<bool>")),
-            category_pair: profile.add_category("Bool", CategoryColor::Blue).into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_bool = profile.intern_frame(thread, f);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<number>")),
-            category_pair: profile.add_category("Number", CategoryColor::Red).into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_number = profile.intern_frame(thread, f);
-        let f = FrameInfo {
-            frame: Frame::Label(profile.intern_string("<string>")),
-            category_pair: profile.add_category("String", CategoryColor::Green).into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_str = profile.intern_frame(thread, f);
-        let root_frame = FrameInfo {
-            frame: Frame::Label(profile.intern_string("json")),
-            category_pair: object_category.into(),
-            flags: FrameFlags::empty(),
-        };
-        let f_root = profile.intern_frame(thread, root_frame);
-        let s_root = profile.intern_stack(thread, None, f_root);
+        let cat_obj = profile.add_category("Object", CategoryColor::Gray);
+        let cat_arr = profile.add_category("Array", CategoryColor::Gray);
+        let cat_null = profile.add_category("Null", CategoryColor::Yellow);
+        let cat_bool = profile.add_category("Bool", CategoryColor::Blue);
+        let cat_number = profile.add_category("Number", CategoryColor::LightBlue);
+        let cat_str = profile.add_category("String", CategoryColor::Green);
+        let top_label = profile.intern_string("json");
+        let top_category = cat_obj;
         Self {
             profile,
             thread,
-            stack: vec![s_root],
+            stack: Vec::new(),
+            stack_labels: Vec::new(),
+            top_label,
+            top_category,
+            string_stack: vec!["json".into()],
             scope_stack: Vec::new(),
-            object_category,
-            f_obj,
-            f_arr,
-            f_null,
-            f_bool,
-            f_number,
-            f_str,
+            cat_obj,
+            cat_arr,
+            cat_null,
+            cat_bool,
+            cat_number,
+            cat_str,
             last_pos: 0,
             aggregate_depth,
             aggregation_map: None,
+            array_depth: 0,
         }
     }
 
@@ -168,23 +140,39 @@ impl State {
             return;
         }
         let delta = pos - self.last_pos;
-        let stack_handle = self.stack.last().cloned();
+        let top_frame = self.profile.intern_frame(
+            self.thread,
+            FrameInfo {
+                frame: Frame::Label(self.top_label),
+                category_pair: self.top_category.into(),
+                flags: FrameFlags::empty(),
+            },
+        );
+        let parent_stack = self.stack.last().cloned();
+        let stack_handle = self
+            .profile
+            .intern_stack(self.thread, parent_stack, top_frame);
         if let Some((_, map)) = &mut self.aggregation_map {
-            *map.entry(stack_handle.unwrap()).or_insert(0) += delta;
+            *map.entry(stack_handle).or_insert(0) += delta;
         } else {
             let start_timestamp = Timestamp::from_nanos_since_reference(self.last_pos * 1000);
             let end_timestamp = Timestamp::from_nanos_since_reference(pos * 1000);
             self.profile.add_sample(
                 self.thread,
                 start_timestamp,
-                stack_handle,
+                Some(stack_handle),
                 CpuDelta::ZERO,
                 0,
             );
             let cpu_delta = CpuDelta::from_micros(delta);
             let weight = delta as i32;
-            self.profile
-                .add_sample(self.thread, end_timestamp, stack_handle, cpu_delta, weight);
+            self.profile.add_sample(
+                self.thread,
+                end_timestamp,
+                Some(stack_handle),
+                cpu_delta,
+                weight,
+            );
         }
         self.last_pos = pos;
     }
@@ -218,6 +206,61 @@ impl State {
         assert_eq!(self.last_pos, synth_last_pos);
     }
 
+    fn push_scope(&mut self, scope: Scope) {
+        self.scope_stack.push(scope);
+        if self.scope_stack.len() == self.aggregate_depth {
+            self.aggregation_map = Some((self.last_pos, HashMap::new()));
+        }
+        self.top_category = match scope {
+            Scope::Object => self.cat_obj,
+            Scope::Array => self.cat_arr,
+        };
+    }
+
+    fn pop_scope(&mut self) {
+        if self.scope_stack.len() == self.aggregate_depth {
+            self.consume_aggregation();
+        }
+        self.scope_stack.pop();
+        if let Some(parent_scope) = self.scope_stack.last().cloned() {
+            self.top_category = match parent_scope {
+                Scope::Object => self.cat_obj,
+                Scope::Array => self.cat_arr,
+            };
+        }
+    }
+
+    fn push_label(&mut self, label: StringHandle, category: CategoryHandle) {
+        let top_frame = FrameInfo {
+            frame: Frame::Label(self.top_label),
+            category_pair: self.top_category.into(),
+            flags: FrameFlags::empty(),
+        };
+        let top_frame_handle = self.profile.intern_frame(self.thread, top_frame);
+        let new_stack_handle =
+            self.profile
+                .intern_stack(self.thread, self.stack.last().cloned(), top_frame_handle);
+        self.stack.push(new_stack_handle);
+        self.stack_labels.push((self.top_label, self.top_category));
+        self.top_label = label;
+        self.top_category = category;
+    }
+
+    fn pop_label(&mut self) {
+        self.stack.pop().unwrap();
+        let (label, category) = self.stack_labels.pop().unwrap();
+        self.top_label = label;
+        self.top_category = category;
+    }
+
+    fn maybe_end_property(&mut self) {
+        if self.scope_stack.last() == Some(&Scope::Object) {
+            // Pop the current property from the stack.
+            self.pop_label();
+            self.string_stack.pop();
+        }
+    }
+
     pub fn finish(self) -> Profile {
         self.profile
     }
@@ -226,71 +269,50 @@ impl State {
 impl JsonSessionObserver for State {
     fn begin_object(&mut self, pos_at_obj_start: u64) -> Result<(), String> {
         self.advance(pos_at_obj_start);
-        let new_stack_handle =
-            self.profile
-                .intern_stack(self.thread, self.stack.last().cloned(), self.f_obj);
-        self.stack.push(new_stack_handle);
-        self.scope_stack.push(Scope::Object);
-        if self.scope_stack.len() == self.aggregate_depth {
-            self.aggregation_map = Some((self.last_pos, HashMap::new()));
-        }
+        self.push_scope(Scope::Object);
         Ok(())
     }
 
     fn object_property(&mut self, pos: u64, property_name: String) -> Result<(), String> {
         self.advance(pos);
-        let s = self.profile.intern_string(&property_name);
-        let frame = self.profile.intern_frame(
-            self.thread,
-            FrameInfo {
-                frame: Frame::Label(s),
-                category_pair: self.object_category.into(),
-                flags: FrameFlags::empty(),
-            },
-        );
-        let new_stack_handle =
-            self.profile
-                .intern_stack(self.thread, self.stack.last().cloned(), frame);
-        self.stack.push(new_stack_handle);
+
+        let parent_string = self.string_stack.last().unwrap();
+        let concat_string = format!("{parent_string}.{property_name}");
+        let label = self.profile.intern_string(&concat_string);
+        self.string_stack.push(concat_string);
+        self.push_label(label, self.cat_obj);
+
         Ok(())
     }
 
     fn end_object(&mut self, pos_after_obj_end: u64) -> Result<(), String> {
         self.advance(pos_after_obj_end);
-        self.stack.pop();
-        if self.scope_stack.len() == self.aggregate_depth {
-            self.consume_aggregation();
-        }
-        self.scope_stack.pop();
-        if self.scope_stack.last() == Some(&Scope::Object) {
-            self.stack.pop(); // pop property
-        }
+        self.pop_scope();
+        self.maybe_end_property();
         Ok(())
     }
 
     fn begin_array(&mut self, pos_at_array_start: u64) -> Result<(), String> {
         self.advance(pos_at_array_start);
-        let new_stack_handle =
-            self.profile
-                .intern_stack(self.thread, self.stack.last().cloned(), self.f_arr);
-        self.stack.push(new_stack_handle);
-        self.scope_stack.push(Scope::Array);
-        if self.scope_stack.len() == self.aggregate_depth {
-            self.aggregation_map = Some((self.last_pos, HashMap::new()));
-        }
+
+        self.push_scope(Scope::Array);
+
+        const INDEXER_CHARS: &str = "ijklmnopqrstuvwxyz";
+        let indexer = &INDEXER_CHARS[(self.array_depth % INDEXER_CHARS.len())..][..1];
+        let parent_string = self.string_stack.last().unwrap();
+        let concat_string = format!("{parent_string}[{indexer}]");
+        self.string_stack.push(concat_string);
+        self.array_depth += 1;
+
         Ok(())
     }
 
     fn end_array(&mut self, pos_after_array_end: u64) -> Result<(), String> {
         self.advance(pos_after_array_end);
-        self.stack.pop();
-        if self.scope_stack.len() == self.aggregate_depth {
-            self.consume_aggregation();
-        }
-        self.scope_stack.pop();
-        if self.scope_stack.last() == Some(&Scope::Object) {
-            self.stack.pop(); // pop property
-        }
+        self.string_stack.pop();
+        self.array_depth -= 1;
+        self.pop_scope();
+        self.maybe_end_property();
         Ok(())
     }
 
@@ -301,21 +323,20 @@ impl JsonSessionObserver for State {
         value: JsonPrimitiveValue,
     ) -> Result<(), String> {
         self.advance(pos_before);
-        let entry = match value {
-            JsonPrimitiveValue::Number(_) => self.f_number,
-            JsonPrimitiveValue::Boolean(_) => self.f_bool,
-            JsonPrimitiveValue::String(_) => self.f_str,
-            JsonPrimitiveValue::Null => self.f_null,
+
+        let category = match value {
+            JsonPrimitiveValue::Number(_) => self.cat_number,
+            JsonPrimitiveValue::Boolean(_) => self.cat_bool,
+            JsonPrimitiveValue::String(_) => self.cat_str,
+            JsonPrimitiveValue::Null => self.cat_null,
         };
-        let new_stack_handle =
-            self.profile
-                .intern_stack(self.thread, self.stack.last().cloned(), entry);
-        self.stack.push(new_stack_handle);
+        let prev_category = std::mem::replace(&mut self.top_category, category);
+        self.top_category = category;
         self.advance(pos_after);
-        self.stack.pop();
-        if self.scope_stack.last() == Some(&Scope::Object) {
-            self.stack.pop(); // pop property
-        }
+        self.top_category = prev_category;
+
+        self.maybe_end_property();
+
         Ok(())
     }
 }
